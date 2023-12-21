09b657844fbf12efd62a5dc9c8f5754e68c8558f
GitHub Action <action@github.com>
2023-12-21 15:57:13 +0000

    Add dist directory [skip ci]

--- docs/_nuxt/about-cafe-app.8a2c7f53.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,x as t,v as o}from"./entry.625561b1.js";const n={name:"AboutCafeApp"};function a(r,c,p,s,_,f){return o(),t("div",null,"nothing here")}const i=e(n,[["render",a]]);export{i as default};
