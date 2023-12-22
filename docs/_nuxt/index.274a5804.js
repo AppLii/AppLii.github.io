6cf5d89bc5d0d4d6c72f54cd2b756255d440847a
GitHub Action <action@github.com>
2023-12-22 01:18:21 +0000

    Add dist directory [skip ci]

---
+++ docs/_nuxt/index.274a5804.js
@@ -0,0 +1,1 @@
+import{_ as e,x as n,v as a}from"./entry.e81b0d0d.js";const t={name:"MainPage"};function o(c,r,s,p,_,i){return a(),n("div",null,"index page")}const f=e(t,[["render",o]]);export{f as default};
--- docs/_nuxt/index.caca7ec7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,x as n,v as a}from"./entry.c307959f.js";const t={name:"MainPage"};function o(c,r,s,p,_,i){return a(),n("div",null,"index page")}const f=e(t,[["render",o]]);export{f as default};
