f2081b1287901ff2f30c02568f4e2359cb63ad7e
GitHub Action <action@github.com>
2024-01-02 01:55:53 +0000

    Add dist directory [skip ci]

--- docs/_nuxt/about-cafe-app.5170889d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,x as o,v as s,F as n,y as e}from"./entry.4a6a3622.js";const a={name:"AboutCafeApp"},c=e("p",null,"このページでは、私たちがリリースしたアプリ・現在開発中のアプリを紹介します。",-1),_=e("h1",null,"大学食堂混雑度アプリ(仮名)",-1),p=e("p",null,"現在開発中",-1);function r(l,u,d,f,i,m){return s(),o(n,null,[c,_,p],64)}const x=t(a,[["render",r]]);export{x as default};
