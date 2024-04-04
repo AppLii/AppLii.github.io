e891c1c746f7846d98d43074b59362e1f013414f
Haya <hayachannel5@gmail.com>
2024-04-04 13:29:28 +0900

    変更：テーブルをレスポンシブ対応

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,27 +2,29 @@
     <SideBarComponent>
         <h1>お問い合わせ</h1>
         <p>各種連絡先をご用意しております。お気軽にお問い合わせください。なお、メールアドレスは[アットマーク]を@に置き換えてください。</p>
-        <table>
-            <tr>
-                <th>メールアドレス</th>
-                <td>applii.crea[アットマーク]gmail.com</td>
-            </tr>
-            <tr>
-                <th>お問い合わせ<br>フォーム</th>
-                <td>
-                    <a target="_blank"
-                        href="https://docs.google.com/forms/d/e/1FAIpQLSdDexNyzuBwMtwq5UH6iJ0n_v1moZbEJoJR7BlCK1i5H0i9aQ/viewform?usp=sf_link">
-                        Google Forms</a>（下記のフォームと同じです）
-                </td>
-            </tr>
-            <tr>
-                <th>GitHub</th>
-                <td><a target="_blank" href="https://github.com/AppLii">@AppLii</a></td>
-            </tr>
-            <tr>
-                <th>Twitter（現X）</th>
-                <td><a target="_blank" href="https://twitter.com/applii_crea">@applii_crea</a></td>
-            </tr>
+        <table class="responsive-enabled">
+            <tbody>
+                <tr>
+                    <th>メールアドレス</th>
+                    <td>applii.crea[アットマーク]gmail.com</td>
+                </tr>
+                <tr>
+                    <th>お問い合わせフォーム</th>
+                    <td>
+                        <a target="_blank"
+                            href="https://docs.google.com/forms/d/e/1FAIpQLSdDexNyzuBwMtwq5UH6iJ0n_v1moZbEJoJR7BlCK1i5H0i9aQ/viewform?usp=sf_link">
+                            Google Forms</a>（下記のフォームと同じです）
+                    </td>
+                </tr>
+                <tr>
+                    <th>GitHub</th>
+                    <td><a target="_blank" href="https://github.com/AppLii">@AppLii</a></td>
+                </tr>
+                <tr>
+                    <th>Twitter（現X）</th>
+                    <td><a target="_blank" href="https://twitter.com/applii_crea">@applii_crea</a></td>
+                </tr>
+            </tbody>
         </table>
 
         <h2>お問い合わせフォーム</h2>
